617c07895a14a745ad1874f30606c2d6c661b5a7
Zack Olinger <github@therealzackolinger.com>
2022-11-29 03:51:45 -0600

    Arranged the user profile buttons

--- assets/settings.js
+++ assets/settings.js
@@ -39,53 +39,36 @@ function getCurrentlySavedSettings() {
             var saved_form_element_type = document.getElementById(form_element_id).type
             console.log(`FORM ELEMENT TYPE: ${saved_form_element_type}`)
 
-
-
-            /*
-            WE ARE WORKING ON DETERMINING:
-            1. IF A CHECKBOX IS CHECKED
-            2. SAVING THE VALUE
-            3. RESTORING THE VALUE
-            */
-
-
-
-
             if (saved_form_element_type !== 'checkbox') {
                 document.getElementById(form_element_id).value = saved_form_element_value
             }
 
             if (saved_form_element_type === 'checkbox' && saved_form_element_value === 'true') {
-                //                console.log(`TRUE FORM ELEMENT TYPE: ${saved_form_element_type}`)
-                //                console.log(`TRUE FORM ELEMENT VALUE: ${saved_form_element_value}`)
                 document.getElementById(form_element_id).checked = true
             }
 
             if (saved_form_element_type === 'checkbox' && saved_form_element_value === 'false') {
-                //                console.log(`FALSE FORM ELEMENT TYPE: ${saved_form_element_type}`)
-                //                console.log(`FALSE FORM ELEMENT VALUE: ${saved_form_element_value}`)
                 document.getElementById(form_element_id).checked = false
             }
 
             var form_element_options = getAllElementOptions(form_element_id, saved_form_element_type, saved_form_element_value)
-
-            //            console.log("FORM_ELEMENT_OPTIONS:\n" + JSON.stringify(form_element_options, null, '\t'))
 
             var form_element_settings_object = {
                 form_element_id: form_element_id,
                 form_element_value: saved_form_element_value,
                 form_element_options: form_element_options
             } // END OBJECT
+
             settings_table_objects.push(form_element_settings_object)
 
-            console.log("SETTINGS TABLE OBJECTS:\n" + JSON.stringify(settings_table_objects, null, '\t'))
+            //            console.log("SETTINGS TABLE OBJECTS:\n" + JSON.stringify(settings_table_objects, null, '\t'))
 
         } // END IF
 
 
     }); // END Object.entries
 
-    //    console.log("SETTING_TABLE_OBJECTS:\n" + JSON.stringify(settings_table_objects, null, '\t'))
+
 
 
 
@@ -98,28 +81,7 @@ function getCurrentlySavedSettings() {
             data: settings_table_objects,
             height: "80%",
             //            autoColumns: true,
-            columns: [
-                {
-                    title: "Setting",
-                    field: "form_element_id",
-                    editor: "list",
-                    editorParams: {
-                        editorParams: {
-                            values: ["test", "test1", "test3"]
-                        }
-                    }
-                },
-                {
-                    title: "Value",
-                    field: "form_element_value",
-                    editor: "list",
-                    editorParams: {
-                        editorParams: {
-                            values: ["ttttest", "ttttest1", "ttttest3"]
-                        }
-                    }
-                }
-            ],
+            columns: { title: settings_table_objects[] },
             layout: "fitColumns",
             pagination: true,
             paginationSize: 25,
